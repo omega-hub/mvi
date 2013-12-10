c874b12f3aa0b0d11b30f3c2b733f7932d9d2b5f
Alessandro Febretti <febret@gmail.com>
2013-12-10 02:55:15 -0600

    NEW: WandPointerSwitcher (WIP)

--- mvi.cpp
+++ mvi.cpp
@@ -1,4 +1,15 @@
 #include "WandInputFilter.h"
+#include "WandPointerSwitcher.h"
+
+///////////////////////////////////////////////////////////////////////////////
+// Python API follows
+// this fuction registers the Wand Pointer Switcher service with the omicron
+// service manager
+void setup()
+{
+    ServiceManager* sm = SystemManager::instance()->getServiceManager();
+    sm->registerService("WandPointerSwitcher", (ServiceAllocator)WandPointerSwitcher::New);
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 // Python wrapper code.
@@ -10,5 +21,6 @@ BOOST_PYTHON_MODULE(mvi)
 	PYAPI_REF_BASE_CLASS(WandInputFilter)
 		PYAPI_STATIC_REF_GETTER(WandInputFilter, createAndInitialize)
 		;
+    def("setup", setup);
 }
 #endif
