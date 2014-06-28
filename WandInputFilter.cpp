e00030a1cacfa5af414a24d75d931d0e7e04760a
Alessandro Febretti <febret@gmail.com>
2014-06-28 18:16:21 -0500

    CHANGE: WandInputFilter WIP

--- WandInputFilter.cpp
+++ WandInputFilter.cpp
@@ -30,28 +30,34 @@ void WandInputFilter::update(const UpdateContext& context)
 ///////////////////////////////////////////////////////////////////////////////
 void WandInputFilter::handleEvent(const Event& evt)
 {
-	DisplaySystem* ds = SystemManager::instance()->getDisplaySystem();
-	DisplayConfig& dcfg = ds->getDisplayConfig();
+    if(SystemManager::instance()->isMaster())
+    {
+        DisplaySystem* ds = SystemManager::instance()->getDisplaySystem();
+        DisplayConfig& dcfg = ds->getDisplayConfig();
 
-	// If the event is a wand event, get the ray and see if it intersects any
-	// active tile.
-	if(evt.getServiceType() == Event::ServiceTypeWand)
-	{
-		// Get sensor-space wand ray
-		Ray ray;
-		ray.setOrigin(evt.getPosition());
-		ray.setDirection(evt.getOrientation() * -Vector3f::UnitZ());
+        // If the event is a wand event, get the ray and see if it intersects any
+        // active tile.
+        if(evt.getServiceType() == Event::ServiceTypeWand)
+        {
+            // Get sensor-space wand ray
+            Ray ray;
+            ray.setOrigin(evt.getPosition());
+            ray.setDirection(evt.getOrientation() * -Vector3f::UnitZ());
 
-		// Loop through enabled tiles.
-		typedef KeyValue<String, DisplayTileConfig*> TileItem;
-		foreach(TileItem dtc, dcfg.tiles)
-		{
-			// If we found an intersection, we are done.
-			if(dtc->enabled && dtc->rayIntersects(ray)) return;
-		}
-
-		// No intersection: mark the wand event as processed so it will not
-		// be dispatched to other modules.
-		evt.setProcessed();
-	}
+            // Loop through enabled tiles.
+            typedef KeyValue<String, DisplayTileConfig*> TileItem;
+            foreach(TileItem dtc, dcfg.tiles)
+            {
+                // If we found an intersection, we are done.
+                if(dtc->enabled && dtc->rayIntersects(ray)) 
+                {
+                    return;
+                }
+            }
+            // No intersection: mark the wand event as processed so it will not
+            // be dispatched to other modules.
+            evt.setProcessed();
+            EventSharingModule::markLocal(evt);
+        }
+    }
 }
