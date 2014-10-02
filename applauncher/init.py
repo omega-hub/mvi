9ac31c74f1d4e9fa30bd4bfcd586c0c4def99743
Alessandro Febretti <febret@gmail.com>
2014-10-02 18:57:02 -0500

    NEW: applauncher init

---
+++ applauncher/init.py
@@ -0,0 +1,21 @@
+# Load the workspace configuration script.
+orun('mvi/config/split2.py')
+
+# App launcher always works with stereo disabled.
+getDisplayConfig().forceMono = True
+
+# Set the background picture
+backgroundPicture = 'mvi/applauncher/backgrounds/blue-voron.jpg'
+queueCommand('setBackground()')
+
+#-------------------------------------------------------------------------------
+def setBackground():
+    b = Image.create(UiModule.createAndInitialize().getUi())
+    i = loadImage(backgroundPicture)
+    if(i != None):
+        b.setData(i)
+        b.tile(True)
+        b.setLayer(WidgetLayer.Back)
+        b.setSizeAnchor(Vector2(0,0))
+        b.setSizeAnchorEnabled(True)
+        b.setAutosize(False)
