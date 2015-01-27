9a3654eec1956321613d775c4b24ed85938416ab
Alessandro Febretti <febret@gmail.com>
2015-01-27 16:32:28 -0600

    removed old apps

--- config/split2.py
+++ config/split2.py
@@ -1,19 +1,29 @@
 from mvi import *
 
-wl = WorkspaceLibrary.create()
+def initWorkspace():
+    wl = WorkspaceLibrary.create()
 
-center = wl.createLayout('center')
-center.createWorkspace('full', 'mvi/icons/center-full.png', 0, 0, 1, 1)
+    center = wl.createLayout('center')
+    print('center')
+    print(center)
+    center.createWorkspace('full', 'mvi/icons/center-full.png', 0, 0, 1, 1)
 
-split = wl.createLayout('split')
-split.createWorkspace('left',  'mvi/icons/left-12.png', 0, 0, 0.5, 1)
-split.createWorkspace('right', 'mvi/icons/right-12.png', 0.5, 0, 0.5, 1)
-split.createWorkspace('close', 'mvi/icons/close.png', 0, 0, 0, 0)
+    print('a')
 
-ac = AppController.create()
-ac.setShortcut(EventFlags.ButtonLeft, "split left")
-ac.setShortcut(EventFlags.ButtonRight, "split right")
-ac.setShortcut(EventFlags.ButtonUp, "center full")
-ac.setShortcut(EventFlags.ButtonDown, "split close", "oexit()")
+    split = wl.createLayout('split')
+    split.createWorkspace('left',  'mvi/icons/left-12.png', 0, 0, 0.5, 1)
+    print('b')
+    split.createWorkspace('right', 'mvi/icons/right-12.png', 0.5, 0, 0.5, 1)
+    print('c')
+    split.createWorkspace('close', 'mvi/icons/close.png', 0, 0, 0, 0)
 
-ac.setButton(0, loadImage('mvi/icons/close.png'), 'oexit()')
+    ac = AppController.create()
+    ac.setShortcut(EventFlags.ButtonLeft, "split left")
+    ac.setShortcut(EventFlags.ButtonRight, "split right")
+    ac.setShortcut(EventFlags.ButtonUp, "center full")
+    ac.setShortcut(EventFlags.ButtonDown, "split close", "oexit()")
+
+    ac.setButton(0, loadImage('mvi/icons/close.png'), 'oexit()')
+
+    
+queueCommand('initWorkspace()')
